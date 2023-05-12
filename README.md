63597a1c165be79087a6362752fc28cc2fd0711e
cyclopticnerve <46582023+cyclopticnerve@users.noreply.github.com>
2023-05-12 18:26:07 -0400

    test gui

---
+++ README.md
@@ -0,0 +1,59 @@
+<!----------------------------------------------------------------------------->
+<!-- Project : GUITest                                         /          \  -->
+<!-- Filename: README.md                                      |     ()     | -->
+<!-- Date    : 05/12/2023                                     |            | -->
+<!-- Author  : cyclopticnerve                                 |   \____/   | -->
+<!-- License : WTFPLv2                                         \          /  -->
+<!----------------------------------------------------------------------------->
+
+# GUITest
+
+## "It mostly works™"
+[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)
+
+<!-- __RM_SHORT_DESC_START__ -->
+PP_SHORT_DESC
+<!-- __RM_SHORT_DESC_END__ -->
+
+<!-- ![Screenshot](misc/screenshot.jpg) -->
+
+## Requirements
+<!-- __RM_PY_DEPS_START__ -->
+PP_PY_DEPS
+<!-- __RM_PY_DEPS_END__ -->
+
+## Installing
+You can download the (hopefully stable)
+[latest release](https://github.com/cyclopticnerve/GUITest/releases/latest)
+from the main branch.<br>
+Download either the 'tar.gz' or '.zip' file and extract it.
+
+Then run the 'install.py' file from there:
+```bash
+foo@bar:~$ cd ~/Downloads/GUITest-PP_VERSION
+foo@bar:~/Downloads/GUITest-PP_VERSION$ ./install.py
+```
+
+Or you can clone the git repo to get the latest (and often broken) code from the
+dev branch:
+```bash
+foo@bar:~$ cd ~/Downloads
+foo@bar:~/Downloads$ git clone https://github.com/cyclopticnerve/GUITest
+foo@bar:~/Downloads$ cd GUITest
+foo@bar:~/Downloads/GUITest$ ./install.py
+```
+
+## Uninstalling
+```bash
+foo@bar:~$ cd ~/.cyclopticnerve/guitest
+foo@bar:~/.cyclopticnerve/guitest$ ./uninstall.py
+```
+
+## Usage
+blah blah blah
+
+## Notes
+blah blah blah
+
+## -)
+<!-- -) -->
---
+++ src/guitest.py
@@ -0,0 +1,163 @@
+#! /usr/bin/env python
+# ------------------------------------------------------------------------------
+# Project : GUITest                                                /          \
+# Filename: guitest.py                                            |     ()     |
+# Date    : 05/12/2023                                            |            |
+# Author  : cyclopticnerve                                        |   \____/   |
+# License : WTFPLv2                                                \          /
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
+import argparse
+import os
+import sys
+
+# this is the dir where the script is being run from
+# # (e.g. ~/Documents/Projects/Python/Apps/GUITest)
+dir_curr = os.path.dirname(__file__)
+
+# this is the dir where the gui files are located relative to the script
+# (e.g. ~/Documents/Projects/Python/Apps/GUITest/gui)
+dir_gui = os.path.join(dir_curr, 'gui')
+
+# add gui path to sys path to import handler
+sys.path.insert(0, dir_gui)
+
+import GUITestHandler as handler  # noqa: E402 (import not at top of file)
+
+# ------------------------------------------------------------------------------
+# Constants
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Globals
+# ------------------------------------------------------------------------------
+
+
+# ------------------------------------------------------------------------------
+# Public functions
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# The main function of the program
+# ------------------------------------------------------------------------------
+def main():
+    """
+        The main function of the program
+
+        This function is the main entry point for the program, initializing the
+        program, and performing its steps.
+    """
+
+    # NB: uncomment this line and add args to _add_args()
+    # args = _parse_args()
+
+    # show the window
+    guitestApp = GUITesthandler.GUITestApp()  # noqa: E602 (undefined name)
+    guitestApp.run()
+
+
+# ------------------------------------------------------------------------------
+# Short description
+# ------------------------------------------------------------------------------
+def func():
+    """
+        Short description
+
+        Parameters:
+            var_name [type]: description
+
+        Returns:
+            [type]: description
+
+        Raises:
+            exception_type(vars): description
+
+        Long description (including HTML).
+    """
+
+    return ('this is a test')
+
+
+# ------------------------------------------------------------------------------
+# Add command line args to the parser
+# ------------------------------------------------------------------------------
+def add_args(argparser):
+    """
+        Add command line args to the parser
+
+        Parameters:
+            argparser [ArgumentParser]: the argparse object to add args to
+
+        This function adds arguments to the parser. It is teased out to make
+        editing command line parameters easier.
+    """
+
+    # https://docs.python.org/3/library/argparse.html
+
+    # argparser.add_argument('-f')
+
+
+# ------------------------------------------------------------------------------
+# Private functions
+# ------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
+# Parse command line args and return the dict
+# ------------------------------------------------------------------------------
+def _parse_args():
+    """
+        Parse command line args and return the dict
+
+        Returns:
+            [dict]: the dict of commands passed on the command line
+
+        This function gets the command line passed to the program, parses it,
+        and returns the command line options as a dict.
+    """
+
+    # create the parser
+    argparser = argparse.ArgumentParser(
+        description='PP_SHORT_DESC'
+    )
+
+    # always print prog name/version
+    print('GUITest version PP_VERSION')
+
+    # add arguments from the function above
+    add_args(argparser)
+
+    # parse the arg list into a Namespace object (similar to dict)
+    # NB: if there is an error in the command line, this function will:
+    # 1. print usage
+    # 2. print the error
+    # 3. exit
+    args = argparser.parse_args()
+
+    # if we made it this far, cmd line is ok so print usage
+    argparser.print_usage()
+
+    # convert agrs to dict
+    dict_args = vars(args)
+
+    # return the object for inspection
+    return dict_args
+
+
+# ------------------------------------------------------------------------------
+# Code to run when called from command line
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    """
+        Code to run when called from command line
+
+        This is the top level code of the program, called when the Python file
+        is invoked from the command line.
+    """
+
+    # run main function
+    main()
+
+# -)
